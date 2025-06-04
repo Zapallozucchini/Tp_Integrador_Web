6959cded970ff2712fc5b140d16c47f683d79855
Zapallozucchini <juan.clavero24@gmail.com>
2025-06-03 22:22:05 -0300

    routes
    creamos las rutas que va ausar paciente

---
+++ routes/pacienteRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const pacienteController = require('../controllers/pacienteController');
+
+// Mostrar formulario de nuevo paciente
+router.get('/nuevo', (req, res) => {
+  res.render('pacientes/nuevo');
+});
+
+// Procesar formulario de nuevo paciente
+router.post('/', pacienteController.crearPaciente);
+
+// Listar pacientes
+router.get('/', pacienteController.listarPacientes);
+
+module.exports = router;
